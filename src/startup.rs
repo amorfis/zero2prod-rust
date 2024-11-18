031630f0deeb9149bdfeece9181054b16c3bfb71
Pawel Stawicki <pawelstawicki@gmail.com>
2024-11-18 02:50:13 +0100

    Finished "Telemetry"

--- src/startup.rs
+++ src/startup.rs
@@ -2,7 +2,9 @@ use std::net::TcpListener;
 
 use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
@@ -11,7 +13,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let connection = web::Data::new(db_pool);
 
     let server = HttpServer::new(move || {
-        App::new()
+        App::new().wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Register the connection as part of the application state

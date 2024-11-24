ec3486e0344797fbd4ab32d3ee0e6881bf85889a
Pawel Stawicki <pawelstawicki@gmail.com>
2024-11-25 00:44:53 +0100

    Dockerize and deploy to DigitalOcean

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,11 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
 
-    let connection_pool = PgPool::connect(
+    let connection_pool = PgPool::connect_lazy(
         &configuration.database.connection_string().expose_secret()
     )
-        .await
         .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(address).expect("Failed to bind random port.");
